acd54643b41c9b28cf87ef66f3b88312f2f5c4cc
InterestingTwist <myfavoritebirdcantfly11@gmail.com>
2016-10-28 15:55:14 -0500

    Initial build script

---
+++ buildall.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+OUTDIR=
+SOURCE=
+
+build_all() {
+  OUTDIR=~/COMPLETED
+  SOURCE=~/WORKING_DIRECTORY
+  BEGIN=$(date +%s)
+  ROMS="cm du rr aicp aosip pn screwd omni abc"
+  cd ${SOURCE}
+  for ROM in ${ROMS}
+  do 
+    repo init -u https://github.com/InterestingTwist/${ROM}_manifest.git 
+    repo sync -j4
+    source build/envsetup.sh
+    mka clean
+    # Build the below devices 
+    DEVICES="angler bullhead shamu"
+    for DEVICE in ${DEVICES}
+    do
+      brunch ${DEVICE}
+      mv ${SOURCE}/out/target/product/${DEVICE}/*${DEVICE}*.zip ${OUTDIR}
+      mka clean
+    done
+  done
+  
+    # End
+    END=$(date +%s)
+    cd ~/
+    echo "${green}All builds complete!${reset}"
+    echo "${green}Total time elapsed: $(echo $(($END-$BEGIN)) | awk '{print int($1/60)"mins "int($1%60)"secs "}')${reset}"
+}
